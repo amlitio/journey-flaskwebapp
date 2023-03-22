f2b80f534fd51b64cf46848373ebd01ba0bd9b47
kleos <36254493+amlitio@users.noreply.github.com>
2023-03-22 06:11:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///journal.db'
 db = SQLAlchemy(app)
-from app import db
-db.create_all()
-
 
 class JournalEntry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    content = db.Column(db.String(1000))
+    title = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.String(500), nullable=False)
 
 @app.route('/')
 def index():
@@ -19,14 +16,12 @@ def index():
     return render_template('index.html', entries=entries)
 
 @app.route('/create', methods=['GET', 'POST'])
-def create_entry():
+def create():
     if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-        entry = JournalEntry(title=title, content=content)
+        entry = JournalEntry(title=request.form['title'], content=request.form['content'])
         db.session.add(entry)
         db.session.commit()
-        return redirect(url_for('index'))
+        return redirect('/')
     else:
         return render_template('create.html')
 
